8c6770a98f6259a577925bb7812d7c4e328ee5a7
jjups96 <jjups96@gmail.com>
2018-05-02 21:36:08 -0700

    Testing the tutorial

--- tutorials/MAGIC Gamma Telescope/tpot_MAGIC_Gamma_Telescope_pipeline.py
+++ tutorials/MAGIC Gamma Telescope/tpot_MAGIC_Gamma_Telescope_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import make_pipeline, make_union
 from sklearn.tree import DecisionTreeClassifier
 from tpot.builtins import StackingEstimator
@@ -12,10 +12,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.853347788745
+# Score on the training set was:0.8475988388186995
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=LogisticRegression(C=10.0, dual=False, penalty="l2")),
-    DecisionTreeClassifier(criterion="gini", max_depth=7, min_samples_leaf=5, min_samples_split=7)
+    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=8, min_samples_leaf=20, min_samples_split=9)),
+    BernoulliNB(alpha=0.01, fit_prior=True)
 )
 
 exported_pipeline.fit(training_features, training_target)
--- tutorials/tpot_titanic_pipeline.py
+++ tutorials/tpot_titanic_pipeline.py
@@ -1,15 +1,16 @@
 import numpy as np
-
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
-# NOTE: Make sure that the class is labeled 'class' in the data file
-tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
-features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
-training_features, testing_features, training_classes, testing_classes = \
-    train_test_split(features, tpot_data['class'], random_state=42)
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
 
-exported_pipeline = RandomForestClassifier(bootstrap=False, max_features=0.4, min_samples_leaf=1, min_samples_split=9)
+# Score on the training set was:0.8263454656541851
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=3, max_features=0.6500000000000001, min_samples_leaf=20, min_samples_split=19, n_estimators=100, subsample=0.7000000000000001)
 
-exported_pipeline.fit(training_features, training_classes)
+exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
